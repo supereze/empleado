94698eab0c17118cf6f57056af83fc161497d61c
Ezequiel Flores <supereze.bxb@gmail.com>
2020-04-30 12:29:17 -0500

    [feat] generic list

--- applications/home/urls.py
+++ applications/home/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('prueba/', views.PruebaView.as_view()),
+    path('lista/', views.PruebaListView.as_view()),
 ]
--- applications/home/views.py
+++ applications/home/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
 # Create your views here.
 class PruebaView(TemplateView):
     template_name = 'home/prueba.html'
+
+
+class PruebaListView(ListView):
+    template_name = "home/lista.html"
+    context_object_name = 'listaNumeros'
+    queryset = ['0', '10', '20', '30']
+    
