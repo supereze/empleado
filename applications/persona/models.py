63000141eb219e5a1cefd3f3adfff02d9d821ec5
Ezequiel Flores <supereze.bxb@gmail.com>
2020-04-30 17:56:54 -0500

    [feat] persona model

--- applications/persona/models.py
+++ applications/persona/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+from applications.departamento.models import Departamento
+
 # Create your models here.
+class Empleado(models.Model):
+    """Model definition for Persona"""
+    JOB_CHOICES = (
+        ('0', 'CONTADOR'),
+        ('1', 'ADMINISTRADOR'),
+        ('2', 'ECONOMISTA'),
+        ('3', 'OTRO')
+    )
+    # Contador
+    # Administrador
+    # Economista
+    # otro
+    first_name = models.CharField('Nombres', max_length=60)
+    last_name = models.CharField('Apellidos', max_length=60)
+    job = models.CharField('Trabajo', max_length=1, choices=JOB_CHOICES)
+    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+    #image = models.ImageField(, upload_to=None, height_field=None, width_field=None, max_length=None)
+
+
+    def __str__(self):
+        return str(self.id) + '-' + self.first_name + '-' + self.last_name 
