7378759df43965e239dbc9d55a2152ff1c5ce4b6
Ezequiel Flores <supereze.bxb@gmail.com>
2020-04-30 17:37:59 -0500

    [feat] departamento model

--- applications/departamento/models.py
+++ applications/departamento/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Departamento(models.Model):
+    name = models.CharField('Nombre', max_length=50, editable=False)
+    short_name = models.CharField('Nombre Corto', max_length=20, unique=True)
+    anulate = models.BooleanField('Anulado', default=False)
+
+    def __str__(self):
+        return str(self.id) + '-' + self.name + '-' + self.short_name 
