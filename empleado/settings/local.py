46c05bb2886691cc0b68b575ad0a322abd003bf7
Ezequiel Flores <supereze.bxb@gmail.com>
2020-04-30 18:24:56 -0500

    [feat] add database

--- empleado/settings/local.py
+++ empleado/settings/local.py
@@ -9,8 +9,12 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'empleado',
+        'HOST': '127.0.0.1',
+        'USER': 'postgres',
+        'PASSWORD': 'clariad',
+        'PORT': 5432
     }
 }
 
